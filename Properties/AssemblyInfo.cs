d358f5106fc82990089474cd8c8b06e70ad87da5
7020227649 <68322008+7020227649@users.noreply.github.com>
2026-09-11 17:12:43 +0530

    Fix duplicate assembly metadata

--- Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("SiteShield")]
-[assembly: AssemblyDescription("Windows website allow-list blocker")]
-[assembly: AssemblyCompany("SiteShield")]
-[assembly: AssemblyProduct("SiteShield")]
-[assembly: ComVisible(false)]
